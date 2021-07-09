b9356bfb6c969ca3b43a361adbdb51b60259c7d2
Marat1206 <marikzhekov@gmail.com>
2021-07-09 22:31:46 +0300

    init

---
+++ js/recursion.js
@@ -0,0 +1,34 @@
+// function getFactorial(n){
+//     if (n === 1 || n === 0){
+//         return 1;
+//     }else{
+//         return n * getFactorial(n - 1);
+//     }
+// }
+//
+//
+// console.log(getFactorial(3));
+//
+
+
+// function pow(num, deg){
+//     if (deg === 0){
+//         return 1;
+//     }else{
+//         return num * pow(num, deg - 1 );
+//     }
+// }
+//
+//
+// console.log(pow(2, 5));
+
+
+// function sum(num, num1){
+//     if (num1 === 0){
+//         return num;
+//     }
+//     return sum(num +1, num1 - 1);
+// }
+//
+//
+// console.log(sum(20, 30));
